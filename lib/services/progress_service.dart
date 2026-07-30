6ee9444b409ab2f9da9b993f219ab1d59762ff81
contactfyservices-art <contact.fy.services@gmail.com>
2026-07-30 09:13:15 +0300

    Update progress_service.dart

--- lib/services/progress_service.dart
+++ lib/services/progress_service.dart
@@ -5,15 +5,23 @@ class ProgressService {
   static final ProgressService instance = ProgressService._internal();
 
   int totalStars = 0;
+  int totalPoints = 0;
 
   Future<void> load() async {
     final prefs = await SharedPreferences.getInstance();
     totalStars = prefs.getInt('zanako_total_stars') ?? 0;
+    totalPoints = prefs.getInt('zanako_total_points') ?? 0;
   }
 
   Future<void> addStar() async {
     totalStars += 1;
     final prefs = await SharedPreferences.getInstance();
     await prefs.setInt('zanako_total_stars', totalStars);
+  }
+
+  Future<void> addPoints(int value) async {
+    totalPoints += value;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('zanako_total_points', totalPoints);
   }
 }
