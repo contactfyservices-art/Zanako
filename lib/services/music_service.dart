d0424e9496021442cdf38704b0897f3e4d6f1cc2
contactfyservices-art <contact.fy.services@gmail.com>
2026-07-30 20:22:47 +0300

    Update music_service.dart

--- lib/services/music_service.dart
+++ lib/services/music_service.dart
@@ -1,4 +1,5 @@
 import 'package:audioplayers/audioplayers.dart';
+import 'settings_service.dart';
 
 class MusicService {
   MusicService._internal();
@@ -10,7 +11,11 @@ class MusicService {
     if (_started) return;
     _started = true;
     await _player.setReleaseMode(ReleaseMode.loop);
-    await _player.setVolume(0.15);
+    await _player.setVolume(SettingsService.instance.musicLevel / 10);
     await _player.play(AssetSource('audio/background_music.mp3'));
+  }
+
+  Future<void> applyVolume(int level) async {
+    await _player.setVolume(level / 10);
   }
 }
