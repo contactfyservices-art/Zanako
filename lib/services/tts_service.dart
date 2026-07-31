c921faf58cf20fe1dbcb6569784f1bf228c3a3ca
contactfyservices-art <contact.fy.services@gmail.com>
2026-07-31 09:47:05 +0300

    Update tts_service.dart

--- lib/services/tts_service.dart
+++ lib/services/tts_service.dart
@@ -1,27 +1,29 @@
 import 'package:flutter_tts/flutter_tts.dart';
-import 'settings_service.dart';
 
 class TtsService {
   TtsService._internal();
   static final TtsService instance = TtsService._internal();
   final FlutterTts _tts = FlutterTts();
   bool _ready = false;
+  double _volume = 0.8;
 
   Future<void> init() async {
     if (_ready) return;
     await _tts.setLanguage('fr-FR');
     await _tts.setSpeechRate(0.35);
     await _tts.setPitch(1.15);
-    await _tts.setVolume(SettingsService.instance.voiceLevel / 10);
+    await _tts.setVolume(_volume);
     _ready = true;
+  }
+
+  Future<void> setVolumeLevel(int level) async {
+    _volume = level / 10.0;
+    await _tts.setVolume(_volume);
   }
 
   Future<void> speak(String text) async {
     await init();
-    await _tts.setVolume(SettingsService.instance.voiceLevel / 10);
     await _tts.stop();
     await _tts.speak(text);
   }
-
-  Future<void> stop() async => _tts.stop();
 }
