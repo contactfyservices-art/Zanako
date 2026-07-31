9f26ea58be1d85008b1beed57651b7b109d68cbb
contactfyservices-art <contact.fy.services@gmail.com>
2026-07-31 10:25:06 +0300

    Update settings_service.dart

--- lib/services/settings_service.dart
+++ lib/services/settings_service.dart
@@ -1,26 +1,33 @@
 import 'package:shared_preferences/shared_preferences.dart';
+import 'music_service.dart';
+import 'tts_service.dart';
 
 class SettingsService {
   SettingsService._internal();
   static final SettingsService instance = SettingsService._internal();
-  int musicLevel = 6;
-  int voiceLevel = 8;
+
+  int musicVolume = 2;
+  int voiceVolume = 8;
 
   Future<void> load() async {
     final prefs = await SharedPreferences.getInstance();
-    musicLevel = prefs.getInt('zanako_music_level') ?? 6;
-    voiceLevel = prefs.getInt('zanako_voice_level') ?? 8;
+    musicVolume = prefs.getInt('zanako_music_volume') ?? 2;
+    voiceVolume = prefs.getInt('zanako_voice_volume') ?? 8;
+    await MusicService.instance.setVolumeLevel(musicVolume);
+    await TtsService.instance.setVolumeLevel(voiceVolume);
   }
 
-  Future<void> setMusicLevel(int v) async {
-    musicLevel = v;
+  Future<void> setMusicVolume(int v) async {
+    musicVolume = v;
+    await MusicService.instance.setVolumeLevel(v);
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setInt('zanako_music_level', v);
+    await prefs.setInt('zanako_music_volume', v);
   }
 
-  Future<void> setVoiceLevel(int v) async {
-    voiceLevel = v;
+  Future<void> setVoiceVolume(int v) async {
+    voiceVolume = v;
+    await TtsService.instance.setVolumeLevel(v);
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setInt('zanako_voice_level', v);
+    await prefs.setInt('zanako_voice_volume', v);
   }
 }
